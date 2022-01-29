efc691d574f2c6c9337179a5ba37e1255952b917
Austin Bowen <atbowen2@gmail.com>
2022-01-29 16:46:51 -0500

    Turning roads and simple player controls!
    The player car can be steered using the Left and Right Arrow keys.

--- Assets/Scripts/RoadControl.cs
+++ Assets/Scripts/RoadControl.cs
@@ -6,7 +6,7 @@ public class RoadControl : MonoBehaviour
 {
     [SerializeField]
     private Transform playerCar, playerCarEffect;
-    private Sprite playerCarSprite, playerCarEffectSprite;
+    private SpriteRenderer playerCarSprite, playerCarEffectSprite;
     [SerializeField]
     private Sprite playerCarStraight, playerCarLeft, playerCarHardLeft, playerCarRight, playerCarHardRight, 
                     playerCarCrash, playerCarCrashLeft, playerCarCrashRight;
@@ -39,25 +39,40 @@ public class RoadControl : MonoBehaviour
     [SerializeField]
     private float camHeight, roadLength, roadWidth, screenHalfWidth, roadSegmentLength, roadStartZ, FOV;    // roadwidth is actually half the width
     [SerializeField]
-    private float numTotalSegs, numSegsToDraw;
+    private int numStraightSegs, numSegsToDraw;
+    private int straightSegCounter;         // This is essentially an index for determining where to add the RoadCurves
+    private int rumbleStripCounter;         // Used to determine when a rumble strip is complete and the sprite should be changed for the next one
+    private float curAddedSegmentZ;         // This is for keeping track of the Z position of the near edge of the next RoadSegment to add to the track
+    private int curRoadStripSpriteIndex;
+    private Sprite curRoadStripSprite;
     private float distCamToScreen;
     private float nearEdgeWidthScale, farEdgeWidthScale;
 
     private List<RoadSegment> roadSegments = new List<RoadSegment>();
+    
+    [SerializeField]
+    public List<RoadCurve> roadCurves;
+    [SerializeField]
+    public RoadCurve rdCurveExample;
 
     private int curSegmentIndex;
 
     [SerializeField]
-    private float topSpeed, acceleration, deceleration;
-    private float curPlayerSpeed, camCurZ, playerZoffset, curPlayerPosX;
+    private float topSpeed, acceleration, deceleration, maxTurning;
+    private float curPlayerSpeed, curPlayerTurning, camCurZ, playerZoffset, curPlayerPosX;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerCarSprite = playerCar.GetComponent<SpriteRenderer>();
+
         curPlayerSpeed = 0;
         camCurZ = 0;
         distCamToScreen = (numScreenLines / 2f) / (Mathf.Tan(FOV / 2f));
 
+        curAddedSegmentZ = camCurZ + roadStartZ;
+        straightSegCounter = 0;
+        rumbleStripCounter = 0;
         InitializeRoadStrips();
 
         Debug.Log("# road screen line sprites = " + roadScreenSprites.Count);
@@ -67,8 +82,10 @@ public class RoadControl : MonoBehaviour
     void Update()
     {
         curSegmentIndex = GetCurrentRoadSegmentIndex();
-        ManagePosition();
+        ManagePlayerPosition();
         UpdateRoad();
+
+        Debug.Log(roadSegments.Count);
     }
 
     private void InitializeRoadStrips() {
@@ -85,38 +102,56 @@ public class RoadControl : MonoBehaviour
             roadScreenLines.Add(newStrip.transform);
             roadScreenSprites.Add(newStrip.GetComponent<SpriteRenderer>());
         }
+        
+        curRoadStripSpriteIndex = 0;
+        curRoadStripSprite = roadStripSprites[curRoadStripSpriteIndex];
 
-        int stripCounter = 1;
-        int curRoadStripSpriteIndex = 0;
-        Sprite curRoadStripSprite = roadStripSprites[curRoadStripSpriteIndex];
+        while (straightSegCounter < numStraightSegs) {
 
-        for (int i = 0; i < numTotalSegs; i++) {
+            if (rumbleStripCounter > numSegsPerRumble) {
+                curRoadStripSpriteIndex = (curRoadStripSpriteIndex + 1) % roadStripSprites.Count;
+                curRoadStripSprite = roadStripSprites[curRoadStripSpriteIndex];
+                rumbleStripCounter = 1;
+            }
             RoadSegment newSeg = new RoadSegment();
-            newSeg.EdgeNearZ = camCurZ + roadStartZ + roadSegmentLength * i;
-            newSeg.EdgeFarZ = camCurZ + roadStartZ + roadSegmentLength * (i + 1);
+            //newSeg.EdgeNearZ = camCurZ + roadStartZ + roadSegmentLength * i;
+            //newSeg.EdgeFarZ = camCurZ + roadStartZ + roadSegmentLength * (i + 1);
+            newSeg.EdgeNearZ = curAddedSegmentZ;
+            IncrementSegmentsAndRumbleStrips();
+            newSeg.EdgeFarZ = curAddedSegmentZ;
+            newSeg.Curve = 0;
+            newSeg.Y = 0;
             newSeg.SpriteVariation = curRoadStripSprite;
             roadSegments.Add(newSeg);
 
-            stripCounter++;
-            if (stripCounter > numSegsPerRumble) {
-                curRoadStripSpriteIndex = (curRoadStripSpriteIndex + 1) % roadStripSprites.Count;
-                curRoadStripSprite = roadStripSprites[curRoadStripSpriteIndex];
-                stripCounter = 1;
+            if (roadCurves.Count > 0) {
+                foreach(RoadCurve crv in roadCurves) {
+                    if (crv.segmentInsertIndex == straightSegCounter) {
+                        AddRoadCurve(crv);
+
+                        Debug.Log("hi, " + straightSegCounter);
+                    }
+                }
             }
+            
+            straightSegCounter++;
         }
     }
 
     private int GetCurrentRoadSegmentIndex() {
-        return ((int)Mathf.Floor(camCurZ / roadSegmentLength));
+        return ((int)(Mathf.Floor(camCurZ / roadSegmentLength) % roadSegments.Count));
     }
 
     private void UpdateRoad() {
         Debug.Log(curSegmentIndex);
         int highestScreenLineDrawn = -1;    // First screen line is roadScreenLines[0], so let's use -1 as a reference
 
-        for (int i = curSegmentIndex; i < (Mathf.Min(curSegmentIndex + numSegsToDraw - 1, numTotalSegs)); i++) {
-            int nearEdgeHeight = (int)(numScreenLines - Mathf.Floor(distCamToScreen * camHeight / (roadSegments[i].EdgeNearZ - camCurZ)));
-            int farEdgeHeight = (int)(numScreenLines - Mathf.Floor(distCamToScreen * camHeight / (roadSegments[i].EdgeFarZ - camCurZ)));
+        float x = 0;
+        float dx;
+
+        for (int i = curSegmentIndex; i < (Mathf.Min(curSegmentIndex + numSegsToDraw - 1, numStraightSegs)); i++) {
+            int nearEdgeHeight = (int)(numScreenLines - Mathf.Floor(distCamToScreen * (camHeight - roadSegments[i].Y) / (roadSegments[i].EdgeNearZ - camCurZ)));
+            int farEdgeHeight = (int)(numScreenLines - Mathf.Floor(distCamToScreen * (camHeight - roadSegments[i].Y) / (roadSegments[i].EdgeFarZ - camCurZ)));
             nearEdgeWidthScale = (distCamToScreen / (roadSegments[i].EdgeNearZ - camCurZ));
             farEdgeWidthScale = (distCamToScreen / (roadSegments[i].EdgeFarZ - camCurZ));
 
@@ -126,12 +161,17 @@ public class RoadControl : MonoBehaviour
             nearEdgeHeight -= 130;
             farEdgeHeight -= 130;
 
+            dx = roadSegments[i].Curve;
+
             if (farEdgeHeight > highestScreenLineDrawn && farEdgeHeight < numScreenLines) {
                 if (farEdgeHeight > nearEdgeHeight) {
                     for (int j = nearEdgeHeight; j <= farEdgeHeight; j++) {
+                        float dxFraction = (j - nearEdgeHeight) * (dx / (farEdgeHeight - nearEdgeHeight));
                         float scaleIncreaseFromNearEdge = (j - nearEdgeHeight) * ((farEdgeWidthScale - nearEdgeWidthScale) / (farEdgeHeight - nearEdgeHeight));
                         if (j >= 0) {
                             roadScreenSprites[j].sprite = roadSegments[i].SpriteVariation;
+                            roadScreenLines[j].position = new Vector3(x + dxFraction,
+                                                                    roadScreenLines[j].position.y, roadScreenLines[j].position.z);
                             roadScreenLines[j].localScale = new Vector3(nearEdgeWidthScale + scaleIncreaseFromNearEdge, 1.0f, 1.0f);
                             Debug.Log(roadScreenLines[j].localScale.x);
                         }
@@ -140,16 +180,76 @@ public class RoadControl : MonoBehaviour
                 }
                 else {
                     roadScreenSprites[farEdgeHeight].sprite = roadSegments[i].SpriteVariation;
+                    roadScreenLines[farEdgeHeight].position = new Vector3(x + dx,
+                                                                        roadScreenLines[farEdgeHeight].position.y, roadScreenLines[farEdgeHeight].position.z);
                     roadScreenLines[farEdgeHeight].localScale = new Vector3(farEdgeWidthScale, 1.0f, 1.0f);
                     Debug.Log("single seg, screen line #" + nearEdgeHeight);
                 }
             }
 
+            x += dx;
             highestScreenLineDrawn = farEdgeHeight;
+            //Debug.Log(highestScreenLineDrawn);
         }
     }
 
-    private void ManagePosition() {
+    private void AddRoadCurve(RoadCurve rdCurve) {
+
+        float startY = roadSegments[roadSegments.Count - 1].Y;
+        float endY = startY + rdCurve.elevationShift * roadSegmentLength;
+        float total = rdCurve.numEnterSegments + rdCurve.numHoldSegments + rdCurve.numExitSegments;
+
+        for (int i = 0; i < rdCurve.numEnterSegments; i++) {
+            CheckRumbleStripCounter();
+            RoadSegment seg = new RoadSegment();
+            seg.EdgeNearZ = curAddedSegmentZ;
+            IncrementSegmentsAndRumbleStrips();
+            seg.EdgeFarZ = curAddedSegmentZ;
+            seg.Curve = rdCurve.curveIntensity * Mathf.Pow(i / rdCurve.numEnterSegments, 2);
+            seg.Y = startY + (endY - startY) * ((-Mathf.Cos((i / total) * Mathf.PI) / 2) + 0.5f);
+            seg.SpriteVariation = curRoadStripSprite;
+            roadSegments.Add(seg);
+        }
+        for (int i = 0; i < rdCurve.numHoldSegments; i++) {
+            CheckRumbleStripCounter();
+            RoadSegment seg = new RoadSegment();
+            seg.EdgeNearZ = curAddedSegmentZ;
+            IncrementSegmentsAndRumbleStrips();
+            seg.EdgeFarZ = curAddedSegmentZ;
+            seg.Curve = rdCurve.curveIntensity;
+            seg.Y = startY + (endY - startY) * ((-Mathf.Cos(((i + rdCurve.numEnterSegments) / total) * Mathf.PI) / 2) + 0.5f);
+            seg.SpriteVariation = curRoadStripSprite;
+            roadSegments.Add(seg);
+        }
+        for (int i = 0; i < rdCurve.numExitSegments; i++) {
+            RoadSegment seg = new RoadSegment();
+            seg.EdgeNearZ = curAddedSegmentZ;
+            IncrementSegmentsAndRumbleStrips();
+            seg.EdgeFarZ = curAddedSegmentZ;
+            seg.Curve = rdCurve.curveIntensity - rdCurve.curveIntensity * (-Mathf.Cos((i / rdCurve.numEnterSegments) * Mathf.PI) / 2 + 0.5f);
+            seg.Y = startY + (endY - startY) * ((-Mathf.Cos(((i + rdCurve.numEnterSegments + rdCurve.numHoldSegments) / total) * Mathf.PI) / 2) + 0.5f);
+            seg.SpriteVariation = curRoadStripSprite;
+            roadSegments.Add(seg);
+        }
+    }
+
+    private void CheckRumbleStripCounter() {
+        if (rumbleStripCounter > numSegsPerRumble) {
+            curRoadStripSpriteIndex = (curRoadStripSpriteIndex + 1) % roadStripSprites.Count;
+            curRoadStripSprite = roadStripSprites[curRoadStripSpriteIndex];
+            rumbleStripCounter = 1;
+        }
+    }
+
+    private void IncrementSegmentsAndRumbleStrips() {
+        curAddedSegmentZ += roadSegmentLength;
+        rumbleStripCounter++;
+    }
+
+    private void ManagePlayerPosition() {
+
+        curPlayerTurning = maxTurning;
+
         if (Input.GetKey(KeyCode.UpArrow)) {
             if (curPlayerSpeed < topSpeed)  { curPlayerSpeed += acceleration * Time.deltaTime; }
             else                            { curPlayerSpeed = topSpeed; }
@@ -157,6 +257,22 @@ public class RoadControl : MonoBehaviour
         else {
             if (curPlayerSpeed > 0) { curPlayerSpeed -= deceleration * Time.deltaTime; }
             else                    { curPlayerSpeed = 0.0f; }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            playerCarSprite.sprite = playerCarHardLeft;
+            if (Mathf.Abs(playerCar.position.x) < screenHalfWidth) {
+                playerCar.Translate(-curPlayerTurning * Time.deltaTime, 0, 0);
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            playerCarSprite.sprite = playerCarHardRight;
+            if (Mathf.Abs(playerCar.position.x) < screenHalfWidth) {
+                playerCar.Translate(curPlayerTurning * Time.deltaTime, 0, 0);
+            }
+        }
+        else {
+            playerCarSprite.sprite = playerCarStraight;
         }
 
         camCurZ += curPlayerSpeed * Time.deltaTime;
--- Assets/Scripts/RoadSegment.cs
+++ Assets/Scripts/RoadSegment.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class RoadSegment
 {
-    private float edgeNearZ, edgeFarZ;
+    private float edgeNearZ, edgeFarZ, curve, y;
     public float EdgeNearZ { get => edgeNearZ; set => edgeNearZ = value; }
     public float EdgeFarZ { get => edgeFarZ; set => edgeFarZ = value; }
+    public float Curve { get => curve; set => curve = value; }
+    public float Y { get => y; set => y = value; }
     private Sprite spriteVariation;
     public Sprite SpriteVariation { get => spriteVariation; set => spriteVariation = value; }
 }
